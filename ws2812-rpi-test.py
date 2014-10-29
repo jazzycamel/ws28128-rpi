d3814dbd9bbe0befb0e5bfbbbe9367751491b2ca
Rob Kent <jazz_loving_camel@yahoo.co.uk>
2014-10-29 12:12:11 +0000

    Adding license headers

--- ws2812-rpi-test.py
+++ ws2812-rpi-test.py
@@ -1,4 +1,28 @@
 #!/usr/bin/python
+
+###############################################################################
+#                                                                             #
+# WS2812-RPi                                                                  #
+# ==========                                                                  #
+# A C++ library for driving WS2812 RGB LED's (known as 'NeoPixels' by         #
+#     Adafruit) directly from a Raspberry Pi with accompanying Python wrapper #
+# Copyright (C) 2014 Rob Kent                                                 #
+#                                                                             #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#                                                                             #
+###############################################################################
+
 from NeoPixel import NeoPixel
 
 if __name__=="__main__":
@@ -9,4 +33,3 @@ if __name__=="__main__":
     n.show()
     del n
     exit(0)
-
