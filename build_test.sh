24f2ada47cfcd8fa11c47a084279158f00c67d9d
Rob Kent <jazz_loving_camel@yahoo.co.uk>
2014-10-29 12:13:27 +0000

    Adding license headers

--- build_test.sh
+++ build_test.sh
@@ -1,1 +1,26 @@
+#!/bin/sh
+
+###############################################################################
+#                                                                             #
+# WS2812-RPi                                                                  #
+# ==========                                                                  #
+# A C++ library for driving WS2812 RGB LED's (known as 'NeoPixels' by         #
+#     Adafruit) directly from a Raspberry Pi with accompanying Python wrapper #
+# Copyright (C) 2014 Rob Kent                                                 #
+#                                                                             #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#                                                                             #
+###############################################################################
+
 g++ ws2812-rpi.cpp ws2812-rpi-test.cpp -o ws2812-rpi-test
