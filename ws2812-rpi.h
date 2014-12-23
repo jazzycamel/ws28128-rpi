31e17c0f4b6ddd1c46bf9d8a2ee8026565393137
Rob Kent <jazz_loving_camel@yahoo.co.uk>
2014-12-23 18:33:02 +0000

    Update ws2812-rpi.h

--- ws2812-rpi.h
+++ ws2812-rpi.h
@@ -1,3 +1,27 @@
+/*
+###############################################################################
+#                                                                             #
+# WS2812-RPi                                                                  #
+# ==========                                                                  #
+# A C++ library for driving WS2812 RGB LED's (known as 'NeoPixels' by         #
+#     Adafruit) directly from a Raspberry Pi with accompanying Python wrapper #
+# Copyright (C) 2014 Rob Kent                                                 #
+#                                                                             #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#                                                                             #
+###############################################################################
+*/
 #ifndef WS2812_RPI_H
 #define WS2812_RPI_H
 
